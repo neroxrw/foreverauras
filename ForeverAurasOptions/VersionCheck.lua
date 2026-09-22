dd7da52b119938c6cbc645a253df9bd5051a925f
neroxrw <neroxrw@gmail.com>
2026-09-21 22:11:26 -0400

    v0.1.161 (#7)

--- ForeverAurasOptions/VersionCheck.lua
+++ ForeverAurasOptions/VersionCheck.lua
@@ -6,7 +6,7 @@ local Private = select(2, ...)
 
 local L = ForeverAuras.L
 
-local optionsVersion = "0.1.160"
+local optionsVersion = "0.1.161"
 
 
 if optionsVersion ~= ForeverAuras.versionString then
